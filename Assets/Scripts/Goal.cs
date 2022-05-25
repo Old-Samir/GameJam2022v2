8b5de49d9e19062cddb9e75fee495cc7f7aba122
El_Samir <elsamirgame@gmail.com>
2022-05-25 19:36:17 -0400

    Updated scene selector to go to credits after beating final level.

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -58,7 +58,7 @@ public class Goal : MonoBehaviour
 
         if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
         {
-            nextSceneIndex = 0;
+            nextSceneIndex = 1;
             DestroyAllGameObjects();
         }
 
